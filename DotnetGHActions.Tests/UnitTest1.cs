5ffad5c2857882dae8d021ba84767bbaf2b6233f
Maciek Misztal <mmisztal1980@gmail.com>
2020-12-23 22:45:22 +0100

    testing

---
+++ DotnetGHActions.Tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+using System;
+using Xunit;
+
+namespace DotnetGHActions.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Fact]
+        public void Test2()
+        {
+
+        }
+
+        [Fact]
+        public void Test3()
+        {
+
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void Test4(int x)
+        {
+
+        }
+    }
+}
